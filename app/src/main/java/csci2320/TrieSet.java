e1fe18da8a2be35c5236b157bcc6bf8dc27fa621
Mark Lewis <mlewis@trinity.edu>
2023-11-11 15:06:04 -0600

    First commit. Initial test code.

---
+++ app/src/main/java/csci2320/TrieSet.java
@@ -0,0 +1,44 @@
+package csci2320;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class TrieSet implements Iterable<String> {
+  // TODO: Add your Node and private data here.
+
+  public boolean add(String str) {
+    // TODO: Implement this.
+    throw new UnsupportedOperationException();
+  }
+
+  public boolean remove(String str) {
+    // TODO: Implement this.
+    throw new UnsupportedOperationException();
+  }
+
+  public boolean contains(String str) {
+    // TODO: Implement this.
+    throw new UnsupportedOperationException();
+  }
+
+  public int size() {
+    // TODO: Implement this.
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Iterator<String> iterator() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+  }
+
+  public String longestPrefix(String str) {
+    // TODO: Implement this.
+    throw new UnsupportedOperationException();
+  }
+
+  public List<String> validSuffixes(String str) {
+    // TODO: Implement this.
+    throw new UnsupportedOperationException();
+  }
+}
